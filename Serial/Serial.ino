662958b830f8be12bd22510f61b7af70822bb01d
Tominano <martontamas@caesar.elte.hu>
2019-02-28 06:55:06 +0100

    working shorter, waiting for test \..

--- Serial/Serial.ino
+++ Serial/Serial.ino
@@ -1,33 +1,31 @@
-
 int outvec[] = {};
 int pinvec[] = {};
 int j = 100;
 
-
-void setup() 
-{
-  for(int j = 0; j < 7; j++)
+void setup() {
+    for(int j = 0; j < 7; j++)  /*Fill the outvec*/
     int outvec[] = {j};
-  
 
   for(int i = 7; i <= 13; i++){ 
-      pinMode(pinvec[i], OUTPUT);
+      pinMode(pinvec[i], OUTPUT);  /*Fill the pinvec and set those like outputs*/
       }    
       Serial.begin(9600);
       while (!Serial);
-      Serial.println("Input 1 to 7  to turn on the leds or push the 8 and see tha magic delete or push the 9 and look at the runing light.If you type 1--and the number what led you want to read--, you get that is ON or OFF");
+      Serial.println("Input 1 to 7  to turn on the leds or push the 8 and see tha magic delete or push the 9 and look at the runing light.");
+      Serial.println("If you type 1--and the number what led you want to read--, you can see the LED status is ON or OFF");
 }
-void loop()
+
+void loop() 
 
 {
-  if (Serial.available()) 
+  if (Serial.available())   /* https://www.arduino.cc/reference/en/language/functions/communication/serial/available/ */
   {
-    int state = Serial.parseInt();
-    
-    
+    int state = Serial.parseInt();   /*This is the reading from the serial*/
+
+
     if (pinvec[state]== state +6)
     {
-     digitalWrite(pinvec[state], HIGH);
+     digitalWrite(pinvec[state], HIGH);   /*You can turn on the LEDs one by one*/
      Serial.println("Command completed LED turned ON");
     }
 
@@ -35,22 +33,23 @@ void loop()
     if (state == 8)
     {
 
-      for (int i = 6; i <= 13; i++) {
+      for (int i = 7; i <= 13; i++) {
         if (j < 1){j = 1;}
-          digitalWrite(i, HIGH);
+          digitalWrite(i, HIGH);   /*You can turn OFF the whole panel with a runing light */
             delay(j);
-              j -=4;
+              j -=1;
                 digitalWrite(i, LOW);
                   delay(0);
       } 
-    }
-    
+
+       }
+
     if (state == 9)
     {
 
-    for (int i = 6; i <= 13; i++) {
+    for (int i = 7; i <= 13; i++) {
       if (j < 1){j = 1;}
-      digitalWrite(i, HIGH);
+      digitalWrite(i, HIGH);   /*You can turn ON the whole panel with a runing light */
         delay(j);
         j -=4;
           digitalWrite(i, HIGH);
@@ -61,20 +60,18 @@ void loop()
 
     if (state == 10)
     {
-     for (int i = 6; i <= 13; i++)
-     digitalWrite(i, LOW);
-     Serial.println("Command completed LED turned OFF");
+     for (int i = 7; i <= 13; i++)
+     digitalWrite(i, LOW);          /*You can turn OFF every LEDs*/
+     Serial.println("Command completed LEDs turned OFF");
     }
 
 
     if (outvec[state-11]== state-11)
     {
-     digitalRead(pinvec[state - 11] == LOW);
-     Serial.println("OFF");}
-      else {Serial.println("ON");}
+     digitalRead(pinvec[state - 11] == LOW);    /*You can scan outputs, what is their status*/
+     Serial.println("This output is NOT working now, this LED is turned OFF!");}
+      else {Serial.println("This output is WORKING now, this LED is turned ON!");}
 
 
   }
 }
-
-  
